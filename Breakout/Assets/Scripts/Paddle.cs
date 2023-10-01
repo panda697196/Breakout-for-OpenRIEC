9b71dd52814b1197db8e2d7ae9c9e60ba32d78a7
panda697196 <764760706@qq.com>
2023-10-02 01:28:36 +0900

    Basic Scene
    Create the basic playground

---
+++ Breakout/Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Paddle : MonoBehaviour
+{
+    public float xMin;
+    public float xMax;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get horizontal movement displacement
+        float xx = Input.GetAxisRaw("Horizontal");
+        if (xx != 0) {
+            Vector3 pos = transform.position;
+            pos.x += xx * Time.deltaTime * speed;
+            //Restricted to a certain range
+            pos.x = Mathf.Clamp(pos.x,xMin,xMax);
+            transform.position = pos;
+        }
+
+    }
+}
